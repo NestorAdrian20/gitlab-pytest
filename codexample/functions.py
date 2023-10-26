b4204fa1645a5b565ea15f66199248bc4572c51e
Néstor Adrián <martinezjimeneznestoradrian@gmail.com>
2023-10-26 00:26:59 -0600

    first commit

---
+++ codexample/functions.py
@@ -0,0 +1,13 @@
+
+def return_value(value):
+    return value
+
+
+def raise_error(msg_kw):
+    raise ValueError(f"Exception {msg_kw} raised")
+
+def factorielle(n):
+    if n == 0:
+        return 1
+    else:
+        return n  * factorielle(n-1)
---
+++ tests/test_assert1.py
@@ -0,0 +1,28 @@
+import pytest
+
+from codexample.functions import return_value, raise_error
+
+
+def test_return_value():
+    value = 5
+    assert return_value(value) == value
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        result = 1 / 0
+
+
+def test_recursion_depth():
+    with pytest.raises(RuntimeError) as excinfo:
+
+        def f():
+            f()
+
+        f()
+    assert "maximum recursion" in str(excinfo.value)
+
+
+def test_match():
+    msg_kw = "123"
+    with pytest.raises(ValueError, match=r".* %s .*" % msg_kw):
+        raise_error(msg_kw)
